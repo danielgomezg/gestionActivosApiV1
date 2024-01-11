b0874fba732199d9b7d9511d3f142cf435e4a324
mauricarvdiaz <mauricio.carvajal@usach.cl>
2024-01-11 00:07:08 -0300

    history by company v2

--- crud/history.py
+++ crud/history.py
@@ -1,6 +1,7 @@
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import Session, joinedload
 from schemas.historySchema import HistorySchema
 from models.history import History
+from models.sucursal import Sucursal
 from fastapi import HTTPException, status
 from sqlalchemy import func, and_, desc
 
@@ -15,8 +16,22 @@ def get_history_all(db: Session, limit: int = 100, offset: int = 0):
 
 def get_history_by_company(db: Session, company_id: int, limit: int = 100, offset: int = 0):
     try:
-        result = db.query(History).filter(History.company_id == company_id).group_by(History.id).order_by(desc(History.id)).offset(offset).limit(limit).all()
-        #print(result)
+        result = (db.query(History)
+                  .options(joinedload(History.sucursal))
+                  .options(joinedload(History.user))
+                  .filter(History.company_id == company_id)
+                  .order_by(desc(History.id))
+                  .offset(offset)
+                  .limit(limit)
+                  .all())
+        print(result)
+        # hacer que result.sucursal = sucursal
+        result = [history.__dict__ for history in result]
+        for history in result:
+            history['sucursal'] = history['sucursal'].__dict__
+            history['sucursal'].pop('_sa_instance_state', None)
+
+
         return result
     except Exception as e:
         raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail=f"Error al obtener historial de companias {e}")
