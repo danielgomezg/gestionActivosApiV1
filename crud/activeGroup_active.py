a29ae243208d0f0ab8f11aa73f60c65e8677333c
mauricarvdiaz <mauricio.carvajal@usach.cl>
2024-06-04 23:47:44 -0400

    add funciones crud coll

--- crud/activeGroup_active.py
+++ crud/activeGroup_active.py
@@ -1,6 +1,8 @@
 from sqlalchemy.orm import Session
 from schemas.activeGroup_active import ActiveGroup_ActiveSchema
 from models.activeGroup_active import Active_GroupActive
+from models.active import Active
+from schemas.activeGroupSchema import ActiveGroupSchema
 from fastapi import HTTPException, status
 from sqlalchemy import and_
 
@@ -37,3 +39,59 @@ def create_activeGroup_active(db: Session, activeGroup_active: ActiveGroup_Activ
         return _activeGroup_active
     except Exception as e:
         raise HTTPException(status_code=status.HTTP_409_CONFLICT,detail=f"Error creando perfilAccion {e}")
+    
+def create_collection_actives(db: Session, body: ActiveGroupSchema, collId: int):
+    try:
+        mapping = [
+            {
+                "activeGroup_id": collId,
+                "active_id": active_id
+            }
+            for active_id in body.activesId
+        ]
+        db.bulk_insert_mappings(Active_GroupActive, mapping)
+        db.commit()
+
+        return mapping
+
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT,detail=f"Error creando perfilAccion {e}")
+
+def get_actives_by_idCollection(db: Session, collId: int):
+    try:
+        result = db.query(Active_GroupActive, Active.id, Active.bar_code, Active.virtual_code).outerjoin(Active, Active.id == Active_GroupActive.active_id).filter(Active_GroupActive.activeGroup_id == collId).all()
+
+        result_dict = [
+            {   
+                "id": id,
+                "bar_code": bar_code, 
+                "virtual_code": virtual_code 
+            }
+            for _, id, bar_code, virtual_code in result
+        ]
+
+        return result_dict
+    
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail=f"Error al buscar actives por id de coleccion {e}")
+    
+def update_collection_actives(db: Session, collId: int, activesId: list):
+    try:
+        # Eliminar todos los actives de la coleccion
+        db.query(Active_GroupActive).filter(Active_GroupActive.activeGroup_id == collId).delete()
+
+        # Insertar los nuevos actives
+        mapping = [
+            {
+                "activeGroup_id": collId,
+                "active_id": active_id
+            }
+            for active_id in activesId
+        ]
+        db.bulk_insert_mappings(Active_GroupActive, mapping)
+        db.commit()
+
+        return mapping
+    
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail=f"Error al actualizar actives de coleccion {e}")
